e531b82f36654b661cf2373d3e4577417fb679c5
Nikolas Júlio <nikolasjuliogarciadejesus@gmail.com>
2023-02-28 13:22:29 -0300

    Terminei o vídeo

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const meuVideo = document.getElementById("video1")
 function playPause() {
     if (meuVideo.paused) {
         meuVideo.play();
+        meuVideo.classList.remove("fa-play")
+        meuVideo.classList.add("fa-pause")
     }
     else
         meuVideo.pause()
 }
+
